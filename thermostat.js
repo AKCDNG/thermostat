4e9bc848fa642e24427f554c334c06d9bb626637
Anish <anishkakaiya@outlook.com>
2022-05-27 16:19:04 +0100

    PSM on added

--- thermostat.js
+++ thermostat.js
@@ -1,6 +1,7 @@
 class Thermostat {
   constructor() {
     this.temperature = 20
+    this.powerSavingMode = true
   }
 
   getTemperature() {
@@ -8,7 +9,11 @@ class Thermostat {
   }
 
   up() {
-    this.temperature += 1
+    if(this.powerSavingMode = true && this.temperature < 25) {
+      this.temperature += 1;
+    } else {
+      return 'PSM is now on, max temperature is 25'
+    }
   }
 
   down() {
@@ -16,6 +21,14 @@ class Thermostat {
       this.temperature -= 1;
     } else {
       return 'Minimum temperature reached';
+    }
+  }
+
+  setPowerSavingMode(input) {
+    if(this.powerSavingMode === true && input === true){
+      return 'Power Saving Mode is already on';
+    } else {
+      this.powerSavingMode = input;
     }
   }
 }
--- thermostat.test.js
+++ thermostat.test.js
@@ -18,13 +18,26 @@ describe('Thermostat', () => {
     expect(thermostat.getTemperature()).toEqual(19);
   })
 
-it('Has a minimum temperature of 10 degrees', () => {
-  const thermostat = new Thermostat;
-  for(let i = 0 ; i < 10 ; i++) {
-    thermostat.down();
-  }
-  expect(thermostat.down()).toEqual('Minimum temperature reached');
-  
-});
+  it('Has a minimum temperature of 10 degrees', () => {
+    const thermostat = new Thermostat;
+    for(let i = 0 ; i < 10 ; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.down()).toEqual('Minimum temperature reached');
+  });
+    
+
+  it('Has a maximum temperature of 25 degrees when Power Saving Mode is on', () => {
+    const thermostat = new Thermostat;
+    for(let i = 0 ; i < 5 ; i++) {
+      thermostat.up(); 
+    }
+    expect(thermostat.up()).toEqual('PSM is now on, max temperature is 25')
+  });
+
+  it('Has power saving mode on by default', () => {
+    const thermostat = new Thermostat;
+    expect(thermostat.setPowerSavingMode(true)).toEqual('Power Saving Mode is already on');
+  });
 
 });
